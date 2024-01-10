5723cecc48fddc24d14e2a2f417e6e2117b45acd
janak <janak@ADAMOVINA>
2024-01-10 13:41:19 +0100

    Vytvorenie vezi

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace Hanojske_Veze
     {
         static void Main(string[] args)
         {
+            Stack<int> vez1 = new Stack<int>();
+            Stack<int> vez2 = new Stack<int>();
+            Stack<int> vez3 = new Stack<int>();
+            string[] kotuc = { "██", "████", "██████", "████████", "██████████" };
+            for (int i = 4; i <= 0; i++)
+            {
+                vez1.Push(i);
+            }
+
+
+
         }
     }
 }
