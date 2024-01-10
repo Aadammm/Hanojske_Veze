5ac46867295e9d0b9aafc879bddf0a0131168f5d
janak <janak@ADAMOVINA>
2024-01-10 13:30:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanojske_Veze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
